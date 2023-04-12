d841ba180678e5fd6c59415b59a25c8bcf139bd9
Taiwo Hassan Yusuf <teehazzan@gmail.com>
2023-04-12 15:31:35 +0100

    Added cache tests

--- src/clients/__tests__/catFactAPI.test.ts
+++ src/clients/__tests__/catFactAPI.test.ts
@@ -5,23 +5,32 @@ import {
 } from "../../clients/catFactAPI";
 import { CatFactAPIArgs } from "@/interfaces/api";
 import config from "@/config";
+import NodeCache from "node-cache";
 
 jest.mock("axios");
+jest.mock("node-cache");
+
+const mockSuccessResponse = {
+  current_page: 1,
+  per_page: 5,
+  last_page: 10,
+  data: [{ fact: "Cats are awesome!", length: 17 }],
+};
+
+const transformedSuccessResponse = {
+  currentPage: 1,
+  perPage: 5,
+  totalPages: 10,
+  facts: [{ fact: "Cats are awesome!", length: 17 }],
+};
+
+const params = {
+  limit: 5,
+  page: 1,
+  maxLength: 140,
+};
 
 describe("GetCatFacts", () => {
-  const mockSuccessResponse = {
-    current_page: 1,
-    per_page: 5,
-    last_page: 10,
-    data: [{ fact: "Cats are awesome!", length: 17 }],
-  };
-
-  const params = {
-    limit: 5,
-    page: 1,
-    maxLength: 140,
-  };
-
   afterEach(() => {
     jest.resetAllMocks();
   });
@@ -59,12 +68,7 @@ describe("GetCatFacts", () => {
     });
 
     expect(result).toEqual({
-      data: {
-        currentPage: 1,
-        perPage: 5,
-        totalPages: 10,
-        facts: [{ fact: "Cats are awesome!", length: 17 }],
-      },
+      data: transformedSuccessResponse,
       isError: false,
     });
   });
@@ -98,5 +102,45 @@ describe("GetCatFacts", () => {
       data: mockSuccessResponse,
     } as any);
     expect(result.data).toEqual(transformedResponse);
+  });
+});
+
+describe("getCatFactsFromCache", () => {
+  const cachedResponse = transformedSuccessResponse;
+
+  let cacheGetSpy: jest.SpyInstance;
+  let cacheSetSpy: jest.SpyInstance;
+
+  beforeEach(() => {
+    cacheGetSpy = jest.spyOn(NodeCache.prototype, "get");
+    cacheSetSpy = jest.spyOn(NodeCache.prototype, "set");
+  });
+
+  afterEach(() => {
+    cacheGetSpy.mockRestore();
+    cacheSetSpy.mockRestore();
+  });
+
+  it("should return the cached response if it exists", async () => {
+    cacheGetSpy.mockReturnValue(cachedResponse);
+
+    const result = await getCatFacts(params as CatFactAPIArgs);
+    expect(result).toEqual({ data: cachedResponse, isError: false });
+    expect(cacheGetSpy).toHaveBeenCalledTimes(1);
+  });
+
+  it("should call the API and set the cache if the cache is empty", async () => {
+    cacheGetSpy.mockReturnValue(undefined);
+    (axios.get as jest.MockedFunction<typeof axios.get>).mockResolvedValue({
+      data: mockSuccessResponse,
+    });
+
+    const result = await getCatFacts(params as CatFactAPIArgs);
+    expect(result).toEqual({
+      data: transformedSuccessResponse,
+      isError: false,
+    });
+    expect(axios.get).toHaveBeenCalledTimes(1);
+    expect(cacheSetSpy).toHaveBeenCalledWith("1-5-140", transformedSuccessResponse);
   });
 });
