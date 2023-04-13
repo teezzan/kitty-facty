a54fff7a68f723f25f8a94e9cf6ea095a307a322
Taiwo Hassan Yusuf <teehazzan@gmail.com>
2023-04-13 11:39:03 +0100

    Added more JS Docs

--- src/utils/catFactUtils.ts
+++ src/utils/catFactUtils.ts
@@ -2,6 +2,11 @@ import config from "@/config";
 import { NextAPIQueryStrings, CatFactAPIArgs, Fact } from "@/interfaces/api";
 import { SortOrder } from "@/interfaces/constants";
 
+/**
+ * Parses a set of query strings and returns an object with the corresponding CatFactAPIArgs values.
+ * @param {NextAPIQueryStrings} query - The set of query strings to parse.
+ * @returns {CatFactAPIArgs} - An object with the corresponding CatFactAPIArgs values.
+ */
 export const parseQueryStrings = (
   query: NextAPIQueryStrings
 ): CatFactAPIArgs => {
@@ -33,6 +38,13 @@ export const parseQueryStrings = (
   };
 };
 
+/**
+ * Adds an ID property to an array of Fact objects, based on the current page and limit.
+ * @param {Fact[]} facts - An array of Fact objects to add an ID property to.
+ * @param {number} currentPage - The current page being displayed. Defaults to 1.
+ * @param {number} limit - The number of facts to display per page. Defaults to the value in the config file.
+ * @returns {Fact[]} - An array of Fact objects with an ID property added to each object.
+ */
 export const addIDToCatFacts = (
   facts: Fact[],
   currentPage: number = 1,
@@ -47,6 +59,12 @@ export const addIDToCatFacts = (
   });
 };
 
+/**
+ * Sorts an array of Fact objects alphabetically.
+ * @param {Fact[]} facts - An array of Fact objects to sort.
+ * @param {boolean} sortAsc - Determines if the sorting is ascending or descending. Defaults to true (ascending).
+ * @returns {Fact[]} - A sorted array of Fact objects.
+ */
 export const sortFactsAlphabetically = (
   facts: Fact[],
   sortAsc = true
@@ -62,6 +80,12 @@ export const sortFactsAlphabetically = (
   });
 };
 
+/**
+ * Sorts an array of Fact objects by length.
+ * @param {Fact[]} facts - An array of Fact objects to sort.
+ * @param {boolean} sortAsc - Determines if the sorting is ascending or descending. Defaults to true (ascending).
+ * @returns {Fact[]} - A sorted array of Fact objects.
+ */
 export const sortFactsByLength = (facts: Fact[], sortAsc = true): Fact[] => {
   return facts.sort((a, b) => {
     if (a.length > b.length) {
@@ -74,6 +98,12 @@ export const sortFactsByLength = (facts: Fact[], sortAsc = true): Fact[] => {
   });
 };
 
+/**
+ * Returns a positive number or a fallback value if the number is not positive.
+ * @param {number|undefined} num - The number to check if it's positive.
+ * @param {number} fallback - The fallback value to use if the number is not positive.
+ * @returns {number} - A positive number or the fallback value.
+ */
 function getPositiveNumberOrDefault(
   num: number | undefined,
   fallback: number
--- src/utils/env.ts
+++ src/utils/env.ts
@@ -2,10 +2,18 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-// Keys is a list of keys that were retrieved from environment.
+/**
+ * The list of keys that were retrieved from the environment
+ * @type {string[]}
+ */
 export const Keys: string[] = [];
 
-// GetString retrieves an environment variable and parses it as string.
+/**
+ * Retrieves an environment variable and parses it as string
+ * @param {string} key - the name of the environment variable to retrieve
+ * @param {string} fallback - the fallback value to use if the environment variable is not set or is invalid
+ * @returns {string} - the value of the environment variable or fallback
+ */
 export function GetString(key: string, fallback: string): string {
   Keys.push(key);
   if (process.env[key]) {
@@ -14,7 +22,12 @@ export function GetString(key: string, fallback: string): string {
   return fallback;
 }
 
-// GetInt retrieves an environment variable and parses it as int.
+/**
+ * Retrieves an environment variable and parses it as an integer
+ * @param {string} key - the name of the environment variable to retrieve
+ * @param {number} fallback - the fallback value to use if the environment variable is not set or is invalid
+ * @returns {number} - the value of the environment variable or fallback
+ */
 export function GetInt(key: string, fallback: number): number {
   Keys.push(key);
   if (process.env[key]) {
@@ -26,7 +39,12 @@ export function GetInt(key: string, fallback: number): number {
   return fallback;
 }
 
-// GetBool retrieves an environment variable and parses it as boolean.
+/**
+ * Retrieves an environment variable and parses it as a boolean
+ * @param {string} key - the name of the environment variable to retrieve
+ * @param {boolean} fallback - the fallback value to use if the environment variable is not set or is invalid
+ * @returns {boolean} - the value of the environment variable or fallback
+ */
 export function GetBool(key: string, fallback: boolean): boolean {
   Keys.push(key);
   if (process.env[key]) {
